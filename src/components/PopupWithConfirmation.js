d29721955416822b1d8f239e96c4ff55b31c6c1c
alex <a.m.mubinov@yandex.ru>
2023-03-17 18:43:35 +0500

    fixed after first review

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -28,9 +28,4 @@ export default class PopupWithConfirmation extends Popup {
         });
         super.setEventListeners();
     }
-
-    //удалить карточку со страницы
-//     deleteCard() {
-//         this._card.remove();
-//     }
-// }
+}
